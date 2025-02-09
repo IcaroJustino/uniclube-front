746e3982732df7c043af4eb4c3ef445401d55af7
Icaro Justino <icarojustinom@hotmail.com>
2025-02-08 21:25:52 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import NotFound from "./pages/not-found";
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+}
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,21 @@
+import { useState, useEffect } from "react";
+import Loading from "../loading-page";
+
+export default function Home() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Verifica se a página já carregou
+    const handleLoad = () => setLoading(false);
+
+    if (document.readyState === "complete") {
+      setLoading(false);
+    } else {
+      window.addEventListener("load", handleLoad);
+    }
+
+    return () => window.removeEventListener("load", handleLoad);
+  }, []);
+
+  return loading ? <Loading /> : <main>Content</main>;
+}
